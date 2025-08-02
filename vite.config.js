75e3290d03c6a3c7e1c7eb388276e543f0f68cf4
David Walstad Haugstvedt <113419628+d4v1dwalstadh@users.noreply.github.com>
2025-08-02 20:42:44 +0200

    vite.config oppdatering

--- vite.config.js
+++ vite.config.js
@@ -4,8 +4,15 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/Smud-Reise/', // Dette må matche repository-navnet ditt
+  
+  // GitHub Pages base path - endre 'smud-reise' til ditt repository-navn
+  base: '/smud-reise/',
+  
+  server: {
+    port: 5173, // Du kan endre porten her hvis ønskelig
+  },
+  
   build: {
-    outDir: 'dist',
+    outDir: 'dist', // Output mappe når du bygger prosjektet
   },
 })
