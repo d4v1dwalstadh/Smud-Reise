4e54b2be25362639188064a60c6908b2e60e8b67
David Walstad Haugstvedt <113419628+d4v1dwalstadh@users.noreply.github.com>
2025-08-02 20:36:08 +0200

    småtteri

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/smud-reise/', // Dette må matche repository-navnet ditt
+  base: '/Smud-Reise/', // Dette må matche repository-navnet ditt
   build: {
     outDir: 'dist',
   },
